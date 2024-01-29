d28859d605fc8c89bb075f65906a44ab0090a455
khvsn <nasir.25477@gmail.com>
2024-01-29 15:56:29 +0800

    KUMPUL PRAKTEK REACT

--- public/services.js
+++ public/services.js
@@ -1,7 +1,7 @@
 const services = [
-    {id: 1, name:'FRONTEND DEVELOPMENT', description:'I design beautiful web iterfaces with Figma and Adove XD', image:'../src/assets/icons/ruler-pen.png'},
-    {id:2, name:'WEB DEVELOPMENT', description:'I create beautiful iterfaces with simple HTML, CSS, & JavaScript and also frameworks like Angular and ReactJS', image:'../src/assets/icons/code.png'},
-    {id:3, name:'MOBILE DEVELOPMENT', description:'I am an expert mobile developer. I have experience using Flutter and React Native.', image:'../src/assets/icons/android.png'}
+    {id: 1, name:'FRONTEND DEVELOPMENT', description:'Memastikan bahwa website memiliki waktu muat yang cepat', image:'../src/assets/icons/ruler-pen.png'},
+    {id:2, name:'WEB DEVELOPMENT', description:'Saya membuat antarmuka yang indah dengan HTML, CSS, & JavaScript sederhana dan juga kerangka kerja seperti Angular dan ReactJS', image:'../src/assets/icons/code.png'},
+    {id:3, name:'MOBILE DEVELOPMENT', description:'Saya seorang pengembang seluler yang ahli. Saya memiliki pengalaman menggunakan Flutter dan React Native.', image:'../src/assets/icons/android.png'}
 ]
 
 export default services;
